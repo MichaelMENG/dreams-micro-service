625a588247117d6cbe8a6d74036b38ee0c7e1434
Michael Meng <dreamstep36@163.com>
2018-10-14 14:58:13 +0800

    update teacher tables.

--- _source/pe_teacher.sql
+++ _source/pe_teacher.sql
@@ -1,10 +1,10 @@
 # 数据库 db_user
 CREATE TABLE pe_teacher (
-  user_id     INT UNSIGNED NOT NULL
+  user_id INT UNSIGNED NOT NULL
   COMMENT '用户ID',
-  intro       VARCHAR(64)  NOT NULL
-  COMMENT '教师介绍',
-  description VARCHAR(512) NOT NULL
-  COMMENT '教师描述',
+  intro   VARCHAR(64)  NOT NULL
+  COMMENT '介绍',
+  stars   INT UNSIGNED NOT NULL
+  COMMENT '星级',
   PRIMARY KEY (user_id)
 );
