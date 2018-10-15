8d1aebe1bddf1a7be2e9af9937339cb420c06d48
Michael Meng <dreamstep36@163.com>
2018-10-15 09:55:14 +0800

    prepare for docker.

---
+++ user-thrift-service/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+mvn clean package -DskipTests -U
+
+docker build -t hub.dreams.com/dreams-micro-service/user-thrift-service:1.0-SNAPSHOT .
+
+docker push hub.dreams.com/dreams-micro-service/user-thrift-service:1.0-SNAPSHOT
